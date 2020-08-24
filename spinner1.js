85b5ee40e8c36c563a2a45ad3f177606effd7fd2
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-24 19:57:44 +0000

    spinner1 animation isng \r

---
+++ spinner1.js
@@ -0,0 +1,40 @@
+process.stdout.write('hello from spinner1.js... \rhey\n');
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n');
+}, 1900);
